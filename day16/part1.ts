3fb3020ab2a3cbe7dd080158e01bc1d5b9dc2de7
Benjamin Goh <masotime@gmail.com>
2023-05-24 06:04:43 -0700

    fucking travelling salesman

--- day16/part1.ts
+++ day16/part1.ts
@@ -15,15 +15,6 @@ type Volcano = {
     maxFlow: number
 };
 
-type Step = {
-    valve: string,
-    valvesOpen: Array<string>,
-    action: 'open' | 'move',
-    targetValve: string
-};
-
-type Path = Array<Step>;
-
 type Distances = {
     [name: string]: {
         [name: string]: number
@@ -100,6 +91,7 @@ function constructDistances(volcano: Volcano): Distances {
 // create a heuristic to determine the worth of a node. The worth is equivalent to the potential
 // pressure that can be released given the remaining time and whether or not the valves near it
 // are closed.
+// KIV: Doesn't work
 function evaluateValveWorth({remainingTime, id, visited, distances, volcano }: { remainingTime: number, id: string, visited: Array<string>, distances: Distances, volcano: Volcano }): number {
     let worth = 0;
 
@@ -118,17 +110,7 @@ function evaluateValveWorth({remainingTime, id, visited, distances, volcano }: {
     return worth;
 }
 
-function solve(volcano: Volcano) {
-
-
-}
-
-export default function() {
-    const volcano = parseInput(input);    
-
-    const distances = constructDistances(volcano);
-    console.table(distances);
-
+function failedSolution({ distances, volcano }: { distances: Distances, volcano: Volcano }): number {
     let remainingTime = 30;
     let totalFlowRate = 0;
     let pressureReleased = 0;
@@ -183,7 +165,93 @@ export default function() {
         position = bestValve;
     }
 
-    console.log(`Total pressure released: ${pressureReleased}`);
+    return pressureReleased;
+}
+
+type Path = {
+    steps: Array<{
+        id: string, // valve located at        
+
+        // derived values
+        timeRemaining: number,
+        flowRate: number,
+    }>,
+
+    // derived globals
+    totalPressureReleased: number,
+    unvisited: Array<string>,
+    finalTotal?: number
+}
+
+// Brute force search
+function solve(volcano: Volcano, distances: Distances): Array<Path> {
+
+    // only consider valves that can be opened
+    const viableOptions: Array<Valve> = Object.values(volcano.valves).filter(valve => valve.flowRate !== 0);
+    console.log(`Maximum search space size = ${viableOptions.length}!`);
+
+    // BFS like sweep, potentially improvable by pruning
+    const initialPath: Path = {
+        steps: [{ id: volcano.start, timeRemaining: 30, flowRate: 0 }],
+
+        totalPressureReleased: 0,
+        unvisited: viableOptions.map(valve => valve.name)
+    }
+
+    const searchSpace: Array<Path> = [initialPath];
+    const solutions: Array<Path> = [];
+
+    while (searchSpace.length > 0) {
+        const path = searchSpace.shift() as Path; // dequeue
+        const lastPosition = path.steps[path.steps.length - 1];
+
+        if (path.unvisited.length === 0) {
+            // we have a "solution"
+            const { flowRate, timeRemaining } = path.steps[path.steps.length - 1];
+            path.finalTotal = path.totalPressureReleased + flowRate * timeRemaining;
+            solutions.push(path);
+            continue;
+        }
+
+        for (const valve of path.unvisited) {            
+            // simulate "teleporting" to that valve and opening it
+            const timeElapsed = distances[lastPosition.id][valve] + 1 // +1 to open it
+            const timeRemaining = lastPosition.timeRemaining - timeElapsed;
+            const flowRate = lastPosition.flowRate + volcano.valves[valve].flowRate;
+            const totalPressureReleased = path.totalPressureReleased + lastPosition.flowRate * timeElapsed;
+            const unvisited = [...path.unvisited];
+
+            unvisited.splice(unvisited.indexOf(valve), 1);
+
+            const nextStep = {
+                id: valve,
+                timeRemaining,
+                flowRate
+            };
+
+            searchSpace.push({
+                steps: [...path.steps, nextStep],
+
+                totalPressureReleased,
+                unvisited
+            });
+        }
+
+        // (searchSpace.length % 1000 <= 100) && console.log(`Search Space is now of size ${searchSpace.length}`);
+    }
+
+    return solutions;
+}
+
+export default function() {
+    const volcano = parseInput(input);    
+
+    const distances = constructDistances(volcano);
+    console.table(distances);
+
+    const solutions = solve(volcano, distances);
+    solutions.sort((path1, path2) => (path2.finalTotal ?? 0) - (path1.finalTotal ?? 0)); // reverse sort
+    console.log(solutions[0]);
 
     return volcano;
 }
